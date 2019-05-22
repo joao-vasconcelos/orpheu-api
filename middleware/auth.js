628588e4ae6d59352744ae7886a4a4a9da17fb0f
João de Vasconcelos <joao@AirJoao.local>
2019-05-22 17:21:42 +0100

    Switched Authentication OFF

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,9 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function(req, res, next) {
+  // Switch authentication ON or OFF based on config file
+  if (!config.get("auth.requiresAuth")) return next();
+
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access denied. No token provided.");
 
