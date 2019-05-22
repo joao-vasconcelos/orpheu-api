b5f1610571de1d6b29eb7d4989c559e573df2d9d
João de Vasconcelos <joao@AirJoao.local>
2019-05-22 17:25:53 +0100

    Switched Admin OFF

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,8 @@
 const config = require("config");
 
 module.exports = function(req, res, next) {
+  if (!config.get("auth.requiresAdmin")) return next();
+
   if (!req.user.isAdmin) return res.status(403).send("Access denied.");
   next();
 };
